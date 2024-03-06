4344d6da1e0553bd211558969876f60bb0c8fcf1
Manish Dusa <manish.dusa211026@tsecol.onmicrosoft.com>
2024-03-06 18:43:23 +0530

    added location on appbar

--- lib/screens/Home/home.dart
+++ lib/screens/Home/home.dart
@@ -1,3 +1,4 @@
+import 'package:dartdash/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,8 +11,39 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("HomePage"),
+    final double Screenheight = MediaQuery.of(context).size.height;
+    final double Screenwidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0), // Customize the height here
+        child: AppBar(
+          title: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Icons.location_on,
+                    size: 15,
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  Text(
+                    'Location',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          shadowColor: black,
+          elevation: 2,
+          backgroundColor: white,
+        ),
+      ),
+      // body:
     );
   }
 }
